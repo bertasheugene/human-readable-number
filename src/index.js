6934a3f700b5d8166f75b1ec48a47a278bc07ea1
Eugene3112 <bertoshevgieniy@gmail.com>
2020-10-25 14:32:00 +0300

    finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function toReadable (number) {
-  
+    let numerals = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    let tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+    if (number == 0) return 'zero';
+    if (number < 100) return twoDigit(number);
+    if (number < 1000) {
+        if (number % 100 == 0) {
+            return numerals[Math.trunc(number / 100)] + ' hundred';
+        } else {
+            return numerals[Math.trunc(number / 100)] + ' hundred ' + twoDigit(number % 100);
+        }
+    }
+
+    function twoDigit(a) {
+        if (a < 20) {
+            return numerals[a];
+        } else if (a % 10 == 0) {
+            return tens[Math.trunc(a / 10)]
+        } else {
+            return tens[Math.trunc(a / 10)] + ' ' + numerals[a % 10];
+        }
+
+    }
 }
